5eb68fb0fd59e88c79b44af47da1d3218357a8d8
Simon Shepard <simon.shepard@gmail.com>
2019-05-31 11:38:03 +0200

    first version of application for sensor device

---
+++ node_modules/azure-iot-device-amqp/index.js
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+'use strict';
+
+var device = require('azure-iot-device');
+var Amqp = require('./lib/amqp.js').Amqp;
+var AmqpWs = require('./lib/amqp_ws.js').AmqpWs;
+
+/**
+ * The `azure-iot-device-amqp` module provides support for the AMQP protocol over secure 
+ * sockets to the device [client]{@link module:azure-iot-device.Client}.
+ *
+ * @module azure-iot-device-amqp
+ * @requires module:azure-iot-device
+ */
+
+module.exports = {
+  Amqp: Amqp,
+  AmqpWs: AmqpWs,
+  clientFromConnectionString: function (connectionString) {
+    return device.Client.fromConnectionString(connectionString, Amqp);
+  }
+};
---
+++ telemetryMonitor.js
@@ -0,0 +1,42 @@
+'use strict';
+ 
+import { clientFromConnectionString } from 'azure-iot-device-amqp';
+import { Message } from 'azure-iot-device';
+
+exports = module.exports = function(sensorCCS811, sensorBME280){
+
+  const connStr = 'HostName=SKS-Environment.azure-devices.net;DeviceId=EnviroPi;SharedAccessKey=koHVWSqRkPdLNY7wt99NNasT6wgVCH4JuvdhTg8yzMA=',
+        client = clientFromConnectionString(connStr);
+
+  client.open(
+    function (err) {
+      if (err) {
+        console.log('Could not connect to IoT Hub: ' + err);
+      } else {
+        console.log('Client connected to IoT Hub');
+     
+        // Create a message and send it to the IoT Hub every second
+        setInterval(function(){
+          sensorBME280.readSensorData()
+            .then((data) => {   
+              sensorCCS811.read_data();
+              let sensor_data = sensorCCS811.get_data();
+              let combinedData = Object.assign({}, data, sensor_data);
+              var JSONdata = JSON.stringify(combinedData);
+              var telemetryMessage = new Message(JSONdata);
+              console.log("Telemetry sent: " + telemetryMessage.getData());
+              //client.sendEvent(telemetryMessage, (data) => console.log(data));
+            })
+            .catch((err) => {
+              console.log(`BME280 read error: ${err}`);
+            });
+     
+          
+        }, 1000);
+      }
+    }
+  );
+
+}
+
+
