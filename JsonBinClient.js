2bf689fb541e2fd820bae5788083b676135cc189
Simon Shepard <simon.shepard@gmail.com>
2019-06-06 10:57:48 +0200

    converted http rewquest to request library

--- JsonBinClient.js
+++ JsonBinClient.js
@@ -1,59 +1,70 @@
 'use strict';
 
-const https = require('https');
+// const https = require('https');
+const request = require('request');
 
 const getBin = (binId) => {
   return new Promise((resolve, reject) => {
-    https.get(`https://api.jsonbin.io/b/${binId}/latest`, (res) => {
-      let body = []
-      res.on('data', (data) => {
-        body.push(data);
-      });
-      res.on('end', () => {
-        resolve(
-          JSON.parse(body.join())
-        );
-      });
+    request(`https://api.jsonbin.io/b/${binId}/latest`, function (error, response, body) {
+      if(error) reject(error)
 
-    }).on('error', error => {
-      reject(error);
+      resolve(JSON.parse(body));
     });
+
+    // https.get(`https://api.jsonbin.io/b/${binId}/latest`, (res) => {
+    //   let body = []
+    //   res.on('data', (data) => {
+    //     body.push(data);
+    //   });
+    //   res.on('end', () => {
+    //     resolve(
+    //       JSON.parse(body.join())
+    //     );
+    //   });
+
+    // }).on('error', error => {
+    //   reject(error);
+    // });
   });
 };
 
 const putBin = (binId, data) => {
   return new Promise((resolve, reject) => {
-    const body = JSON.stringify(data)
 
-    const options = {
-      hostname: 'api.jsonbin.io',
-      port: 443,
-      path: `/b/${binId}`,
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': body.length
-      }
-    }
+    request({ url: `https://api.jsonbin.io/b/${binId}/latest`, method: 'PUT', json: data}, resolve).on('error', reject)
 
-    const req = https.request(options, (res) => {
-      let body = []
-      res.on('data', (data) => {
-        body.push(data);
-      });
-      res.on('end', () => {
-        resolve(
-          JSON.parse(body.join())
-        );
-      });
-    })
+    // const body = JSON.stringify(data);
 
-    req.on('error', (error) => {
-      reject(error);
-    })
+    // const options = {
+    //   hostname: 'api.jsonbin.io',
+    //   port: 443,
+    //   path: `/b/${binId}`,
+    //   method: 'PUT',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Content-Length': body.length
+    //   }
+    // }
 
-    req.write(body)
-    req.end();
+    // const req = https.request(options, (res) => {
+    //   let body = []
+    //   res.on('data', (data) => {
+    //     body.push(data);
+    //   });
+    //   res.on('end', () => {
+    //     resolve(
+    //       JSON.parse(body.join())
+    //     );
+    //   });
+    // })
+
+    // req.on('error', (error) => {
+    //   reject(error);
+    // })
+
+    // req.write(body)
+    // req.end();
+
   });
 }
 
