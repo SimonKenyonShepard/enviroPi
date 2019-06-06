49231db0eca5d29633b12f7311f59e796d35520c
Simon Shepard <simon.shepard@gmail.com>
2019-06-06 10:34:18 +0200

    added missing bin ID

--- telemetryMonitor.js
+++ telemetryMonitor.js
@@ -13,8 +13,8 @@ exports = module.exports = function(sensorCCS811, sensorBME280){
           sensorCCS811.read_data();
           let sensor_data = sensorCCS811.get_data();
           let combinedData = Object.assign({}, data, sensor_data);
-          console.log("Telemetry sent: " + combinedData);
-          JsonBinClient.appendJsonBin(combinedData);
+          console.log("Telemetry sent: " + JSON.stringify(combinedData));
+          JsonBinClient.appendJsonBin('5cf8c37825ffcf4ba635e18d', combinedData);
 
         })
         .catch((err) => {
